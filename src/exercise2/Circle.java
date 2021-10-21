7cca09a24acf92642beec4fda26c94dec553bff2
NikolayG15 <>
2021-10-21 10:50:25 +0000

    Update

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -9,8 +9,8 @@ public class Circle {
 
      double Pi = 3.14;
      double r = input.nextDouble();
-     float C = 2*Pi*r;
-     float A = Pi*r*r;
+     double C = 2*Pi*r;
+     double A = Pi*r*r;
 
      System.out.println("Circumference: " + C);
      System.out.println("Area: " + A );
